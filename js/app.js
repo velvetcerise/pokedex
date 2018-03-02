5d13882e4e8dfc7301d58cbad50b402e633b53a2
velvetcerise <velvet.cerise@hotmail.com>
2018-03-02 00:57:06 -0600

    v1

---
+++ js/app.js
@@ -0,0 +1,34 @@
+ function handleResponse(pokemon) {
+        console.log('the ajax request has finished!');
+        console.log(pokemon);
+ }
+
+//   function addPokemon(pokemon){
+//     console.log(pokemon);
+//    $('#pokemon').html(`<div class='style'>
+//     < img class = "prueba"
+//     https: //dummyimage.com/280x300/d13d1f/ffffff.png&text=pokemon  alt="muestra">
+//     <div class="body">
+//       <h5 class="name">${pokwmon.name || ''}</h5>
+//       <h6>@${pokemon.login}</h6>
+//       <p class="card-text">${pokemon.bio || ''}</p>
+//       <a href="${pokemon.html_url}" class="btn btn-primary">ver más información</a>
+//     </div>
+//   </div>`*/);
+//    }
+
+   $('#searcher').submit(function (e) {
+       e.preventDefault();
+
+       var searchInf = $('#keyword');
+       var getPkmn = searchInf.val; 
+        console.log(searchInf);
+      //objeto de configuración
+      //request
+        $.ajax({ url: `https://pokeapi.co/api/v2/pokedex/1/${getPkmn}`
+        }).done(handleResponse); //el método .done, sustituye onload()
+
+       
+  
+    });
+//limpiando callbacks
